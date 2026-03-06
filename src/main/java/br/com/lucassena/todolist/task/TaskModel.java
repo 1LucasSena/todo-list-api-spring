33efb87972527726b94a51a32e54f69db198a97d
1LucasSena <lucastsena@gmail.com>
2026-03-06 13:42:22 -0300

    feat: implementação do model, repository e controller de tarefas

--- src/main/java/br/com/lucassena/todolist/task/TaskModel.java
+++ src/main/java/br/com/lucassena/todolist/task/TaskModel.java
@@ -1,10 +1,11 @@
-package br.com.lucassena.todolist.user;
+package br.com.lucassena.todolist.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,17 +13,24 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name = "users")
-public class UserModel {
+@Entity(name = "tasks")
+public class TaskModel {
+
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  private String username;
-  private String name;
-  private String password;
+  @Column(length = 50)
+  private String title;
+  private String description;
+  // private boolean completed;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
+  private UUID userId;
 
 }
