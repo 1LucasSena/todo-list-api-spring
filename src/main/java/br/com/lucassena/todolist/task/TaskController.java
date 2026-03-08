12bd6b53349434b8dd0a81475abbe08f328bd31e
1LucasSena <lucastsena@gmail.com>
2026-03-08 00:49:39 -0300

    feat: implementa update parcial, validação de títulos e tratamento global de erros

--- src/main/java/br/com/lucassena/todolist/task/TaskController.java
+++ src/main/java/br/com/lucassena/todolist/task/TaskController.java
@@ -1,15 +1,14 @@
 package br.com.lucassena.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import br.com.lucassena.todolist.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -22,19 +21,17 @@ public class TaskController {
   @PostMapping("/")
   public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
-    // Recupera o ID do usuário autenticado
     System.err.println("ID do usuário autenticado: " + request.getAttribute("idUser"));
     var idUser = request.getAttribute("idUser");
     taskModel.setUserId((UUID) idUser);
 
     var currentDate = LocalDateTime.now();
-    // 1. Valida se as datas não estão no passado
+
     if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body("A data de início / término deve ser maior que a data atual");
     }
 
-    // 2. Valida se o início vem depois do fim (O erro do seu teste atual)
     if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body("A data de início deve ser menor que a data de término");
@@ -42,5 +39,39 @@ public class TaskController {
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+
+    var tasks = this.taskRepository.findByUserId((UUID) idUser);
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Tarefa não encontrada");
+    }
+
+
+    var idUser = request.getAttribute("idUser");
+    if (!task.getUserId().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Usuário não tem permissão para alterar esta tarefa");
+    }
+
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(taskUpdated);
   }
 }
--- src/main/java/br/com/lucassena/todolist/task/TaskModel.java
+++ src/main/java/br/com/lucassena/todolist/task/TaskModel.java
@@ -33,4 +33,11 @@ public class TaskModel {
 
   private UUID userId;
 
+  public void setTitle(String title) throws Exception{
+    if(title.length() > 50) {
+      throw new Exception("O campo títilo de ter máximo 50 caracteres");
+    }
+    this.title = title;
+  }
+
 }
